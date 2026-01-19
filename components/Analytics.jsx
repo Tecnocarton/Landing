32d4f01518c34db0d7b23d8d0e5aeb263cf8b90c
tmaldonadoy <tmaldonadoyevenes@gmail.com>
2026-01-19 14:58:53 -0300

    usar variables de entorno con beforeInteractive para Analytics

--- components/Analytics.jsx
+++ components/Analytics.jsx
@@ -5,39 +5,42 @@ import Script from 'next/script';
 import { useEffect } from 'react';
 import { initializeFirebase } from '../lib/firebase-client';
 
-export default function Analytics() {
-  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
-  const measurementId = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
 
+export default function Analytics() {
   useEffect(() => {
-    // Inicializar Firebase cuando el componente se monta
     initializeFirebase();
   }, []);
 
+  if (!GTM_ID && !GA_MEASUREMENT_ID) {
+    return null;
+  }
+
   return (
     <>
-      {/* Google Tag Manager Script */}
-      {gtmId && (
+      {/* Google Tag Manager - Script principal */}
+      {GTM_ID && (
         <Script
           id="gtm-script"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${gtmId}');
+              })(window,document,'script','dataLayer','${GTM_ID}');
             `,
           }}
         />
       )}
 
-      {/* Google Tag Manager (noscript) */}
-      {gtmId && (
+      {/* Google Tag Manager (noscript) - para usuarios sin JavaScript */}
+      {GTM_ID && (
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
@@ -45,25 +48,23 @@ export default function Analytics() {
         </noscript>
       )}
 
-      {/* Google Analytics a través de Firebase */}
-      {measurementId && (
+      {/* Google Analytics 4 - gtag.js */}
+      {GA_MEASUREMENT_ID && (
         <>
           <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+            id="ga-script"
+            strategy="beforeInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
           />
           <Script
-            id="google-analytics"
-            strategy="afterInteractive"
+            id="ga-config"
+            strategy="beforeInteractive"
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${measurementId}', {
-                  page_path: window.location.pathname,
-                  page_title: document.title,
-                });
+                gtag('config', '${GA_MEASUREMENT_ID}');
               `,
             }}
           />
